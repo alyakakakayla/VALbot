7cc957fd8bea1b09790312664500a17ed98070e7
Kayla Reddick <47913067+alyakakakayla@users.noreply.github.com>
2024-02-20 13:55:07 -0500

    Update README in index.js

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 /*
-This bot uses Discord.js v12.5.3 and started three years ago as a small joke among a small Discord server. It now has over 50 finished commands and more than 20 currently in the making, with much room for more in the forseeable future. This bot currently only supports commands in one particular server, as channel IDs are predefined to come from this one server, but that will soon be changed.
+Not the most professional project to showcase, but all the good (and the bad) within these files has become a major part of my coding career development journey. It's also made me a laughing stock for my friends, so it's a win-win for me.
 
-Some features included:
-- various node packages, including mathjs, humanize-duration
-- map and array collections
-- async functions
-- try/catch statements paired with console logging
-- database managers using better-sqlite3
-- mongoose api
+This bot uses the glorious Discord.js **v12.5.3** library and was first created when I was 15 years old as a small joke among a small group of friends in a small Discord server. It currently features over 50 finished commands and tens more in the making, with room for even more sometime in the future. **I do not regularly update this bot**, as this code was written years ago when I was a child. I could barely understand what I was trying to write half the time back then, and I bet the same stands for now. This bot currently only supports commands in one particular guild, as channel IDs are predefined to come from this one guild. Because the Discord.js library being used is deprecated, slash commands do not work here. (Yet.)
 
-Just a few of the Discord specific features included:
-- role handling
-- invite code tracking
-- error catching in log channels
-- message and reaction collectors
-- currency systems
+**Some features included:**
+- view guild and user info
+- minigames like type test race
+- AFK status commands
+- plenty of random math commands like magic 8 ball, dice roll
+- economy feature supporting user balances and inventories
+- invite code/URL tracking
+- user role handling and reading
+- reaction collectors for vote polls
+- error catching that writes to predefined log channel IDs
+
 */
 
 //require
